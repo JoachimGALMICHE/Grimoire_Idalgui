38b1d0f8db24fbf8076f132540c907ebe5172bce
JoachimGALMICHE <joachim.galmiche@hotmail.fr>
2026-03-04 19:40:38 +0100

    Accueil V1

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends Controller
+{
+    #[Route('/', name: 'app_home')]
+    public function index(): Response
+    {
+        $user = $this->getUser();
+        $roles = $user->getRoles();
+
+        if (in_array('ROLE_SOLAIRE', $roles)) {
+            return $this->render('home/solaire.html.twig');
+        }
+
+        return $this->render('home/lunaire.html.twig');
+    }
+}
